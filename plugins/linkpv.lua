efd2c5d49a65c8725aa74ae9e4512b233a839a2e
ABOL1378 <ABOLFAZL.ANVARI24@gmail.com>
2017-01-08 12:07:49 -0800

    Add files via upload

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -729,16 +729,16 @@ local function run(msg, matches)
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] revoked group link ")
       return export_chat_link(receiver, callback, true)
     end
-    if matches[1] == 'لینک شخصی' then
+    if matches[1] == 'linkpv' then
       if not is_momod(msg) then
-        return "فقط برای مدیران"
+        return "For moderators only!"
       end
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
       if not group_link then 
-        return "اول با دستور لینک جدید , لینک بساز"
+        return "Create a link using /newlink first !"
       end
        savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group link ["..group_link.."]")
-     send_large_msg('user#id'..msg.from.id, "⭕️لینک گروه شما:\n"..group_link)
+     send_large_msg('user#id'..msg.from.id, "Group link:\n"..group_link)
     end
     if matches[1] == 'setowner' then
       if not is_owner(msg) then
@@ -838,7 +838,7 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-  "^لینک شخصی$",
+  "^[!/](linkpv)$",
   "%[(photo)%]",
   "^!!tgservice (.+)$",
   },
